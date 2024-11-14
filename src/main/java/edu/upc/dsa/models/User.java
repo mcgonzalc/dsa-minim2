27b9aa9fa3cd78af7d2361f37a9e5c763d3057c4
mcgonzalc <146757355+mcgonzalc@users.noreply.github.com>
2024-11-14 23:02:17 +0100

    Update User.java

--- src/main/java/edu/upc/dsa/models/User.java
+++ src/main/java/edu/upc/dsa/models/User.java
@@ -95,7 +95,7 @@ public class User {
 
     @Override
     public String toString() {
-        return "User [id=" + id + ", name=" + name + ", surname=" + surname + ", hours=" + email + ", email=" + email + ", birthday=" + birthday + " history=" + history + "]";
+        return "User [id=" + id + ", name=" + name + ", surname=" + surname + ", email=" + email + ", birthday=" + birthday + " history=" + history + "]";
     }
 
 }
